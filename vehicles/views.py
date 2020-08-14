7abb4d366d5ed79131858849a7fdb3f41e6b564d
PT-70 <paulyjd@gmail.com>
2020-08-14 13:12:30 +0000

    added vehicles views and templates

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Vehicle
 
 # Create your views here.
+
+def all_vehicles(request):
+    """view to show all vehicles plus sorting and search queries"""
+
+    vehicles = Vehicle.objects.all()
+
+    context = {
+        'vehicles': vehicles,
+    }
+
+    return render(request, 'vehicles/vehicles.html', context)
