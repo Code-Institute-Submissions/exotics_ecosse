d2f7147042a65062358702cf52afdc0ea6f10313
PT-70 <paulyjd@gmail.com>
2020-08-14 19:23:23 +0000

    fixed vehicle detail error (-s)

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_vehicles, name='vehicles'),
-    path('<vehicle_id>', views.vehicle_detail, name='vehicle_detail'),   
+    path('<vehicle_id>', views.vehicle_detail, name='vehicle_detail'),  
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Vehicle
+from . models import Vehicle
 
 # Create your views here.
 
