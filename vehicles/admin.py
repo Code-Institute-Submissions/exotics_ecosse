b4e1c7c74aa17b230d129beeee1aa058eb77ae73
PT-70 <paulyjd@gmail.com>
2020-08-14 12:57:59 +0000

    updated admin

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Vehicle, Category
 
 # Register your models here.
-admin.site.register(Vehicle)
-admin.site.register(Category)
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Category, CategoryAdmin)
 
