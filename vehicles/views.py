3eb515089a08927fda867f8f3252c7f980870896
PT-70 <paulyjd@gmail.com>
2020-08-18 10:34:44 +0000

    added sorting

--- vehicles/views.py
+++ vehicles/views.py
@@ -21,7 +21,8 @@ def all_vehicles(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 vehicles = vehicles.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
