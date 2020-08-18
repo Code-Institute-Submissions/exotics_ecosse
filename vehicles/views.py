af7dc3ad652b1f09a6d03b87cacbdb21a00dedaf
PT-70 <paulyjd@gmail.com>
2020-08-18 10:06:47 +0000

    added sorting

--- vehicles/views.py
+++ vehicles/views.py
@@ -11,8 +11,23 @@ def all_vehicles(request):
     vehicles = Vehicle.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                vehicles = vehicles.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            vehicles = vehicles.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             vehicles = vehicles.filter(category__name__in=categories)
@@ -27,10 +42,13 @@ def all_vehicles(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             vehicles = vehicles.filter(queries)
 
+    current_sorting =f'{sort}_{direction}'
+
     context = {
         'vehicles': vehicles,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'vehicles/vehicles.html', context)
