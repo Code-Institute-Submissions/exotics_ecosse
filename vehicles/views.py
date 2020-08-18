66fae35a91ebf0897fd5514108b90f2eb19952ad
PT-70 <paulyjd@gmail.com>
2020-08-18 09:07:17 +0000

    added search functionality

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from . models import Vehicle
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_vehicles(request):
     """ A view to show all cars, including sorting and search queries """
 
     vehicles = Vehicle.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('vehicles'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            vehicles = vehicles.filter(queries)
 
     context = {
         'vehicles': vehicles,
+        'search_term': query,
     }
 
     return render(request, 'vehicles/vehicles.html', context)
