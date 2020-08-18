2d73fb8e8841e6e7ad1a8bee9069fa624ba47343
PT-70 <paulyjd@gmail.com>
2020-08-18 13:05:39 +0000

    added add to cart functionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -5,7 +5,7 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    vehicle_count = 0
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -19,7 +19,7 @@ def cart_contents(request):
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'vehicle_count': vehicle_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
