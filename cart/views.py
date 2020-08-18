3d2073f30c6e4a5e5977fe49284dcb8d0d663915
PT-70 <paulyjd@gmail.com>
2020-08-18 11:35:20 +0000

    added cart app urls and template

---
+++ cart/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def view_cart(request):
+    """view to return cart contents page"""
+
+    return render(request, 'cart/cart.html')
