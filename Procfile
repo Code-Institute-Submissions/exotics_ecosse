1bf703e59e33bcf7fc5099780e483a56039a17c8
PT-70 <paulyjd@gmail.com>
2020-08-27 09:55:01 +0000

    updated heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn exotics_ecosse.wsgi:application
--- exotics_ecosse/settings.py
+++ exotics_ecosse/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'y*-!r)=2%jsr$8z+c79pil5)@_#-7t)40btk86s!+^^3a()+2m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pt70-exotics-ecosse.heroku.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'exotics_ecosse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-     'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
