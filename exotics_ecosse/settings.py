67e41d026b8dc2be252eb761a809748216b9d223
PT-70 <paulyjd@gmail.com>
2020-08-27 10:12:50 +0000

    removed secret key and set debug

--- exotics_ecosse/settings.py
+++ exotics_ecosse/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y*-!r)=2%jsr$8z+c79pil5)@_#-7t)40btk86s!+^^3a()+2m'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pt70-exotics-ecosse.herokuapp.com', 'localhost']
 
