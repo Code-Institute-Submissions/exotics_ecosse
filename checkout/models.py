64a67cfbf0d71c3fcbcafa3d80ddd6113a4ff394
PT-70 <paulyjd@gmail.com>
2020-09-12 09:02:57 +0000

    adjust 2 line lengths

--- checkout/models.py
+++ checkout/models.py
@@ -68,7 +68,7 @@ class OrderLineItem(models.Model):
     vehicle = models.ForeignKey(Vehicle, null=False,
                                 blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField (max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
