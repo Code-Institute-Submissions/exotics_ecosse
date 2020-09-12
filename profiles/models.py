e4bd8c31da64f3082211f2bfb87ef7f5bbf56971
PT-70 <paulyjd@gmail.com>
2020-09-12 15:07:03 +0000

    update profile py files

--- profiles/models.py
+++ profiles/models.py
@@ -8,8 +8,8 @@ from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
     """
-    user profile model for maintaining
-    del info and booking history
+    A user profile model for maintaining default
+    delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
