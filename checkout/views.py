13f7d57038ea827d0130749ff8a0695180dc1b5c
PT-70 <paulyjd@gmail.com>
2020-09-13 11:08:22 +0000

    final refactor and tidy of py files

--- checkout/views.py
+++ checkout/views.py
@@ -30,7 +30,7 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
-        
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -105,7 +105,8 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # Attempt to prefill the form with any info the user maintains in their profile
+        # Attempt to prefill the form with any
+        #  info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
