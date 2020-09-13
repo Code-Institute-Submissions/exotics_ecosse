14e91fb9d9bc5eb52f37090dc570a665e9e7f66d
PT-70 <paulyjd@gmail.com>
2020-09-13 11:48:17 +0000

    debug false

--- exotics_ecosse/settings.py
+++ exotics_ecosse/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['pt70-exotics-ecosse.herokuapp.com', 'localhost']
 
